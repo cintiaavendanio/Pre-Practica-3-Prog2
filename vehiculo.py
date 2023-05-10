a291fa01440dc1c60641efc01c113c43cda84f3b
cinti.av <cin899a@gmail.com>
2023-05-10 12:11:24 -0300

    prePractica3

--- vehiculo.py
+++ vehiculo.py
@@ -1,31 +1,36 @@
-class Vehiculo():  # Definicion de la clase. Esto es el estado incial
-    marca = str
-    ruedas = int
-    color = str
-    enMarcha = bool
+class Vehiculo():  # Definicion de la clase. 
 
-def __init__(self,nombre,ruedas,color):
-    self.nombre=nombre
-    self.ruedas=ruedas
-    self.color=color
+    def __init__(self,nombre,ruedas,color): #Constructor.
+        self.nombre=nombre
+        self.ruedas=ruedas
+        self.color=color
+        self.enMarcha=False
 
-
-
-def arrancar(self):
-    self.enMarcha = True
+    def arrancar(self):
+        self.enMarcha = True
 
 
-def tipoVehiculo(self):
-    if self.ruedas == 4:
-        return "automovil"
-    elif self.ruedas == 2:
-        return "motocicleta"
+    def tipoVehiculo(self):
+        if self.ruedas == 4:
+            return "automovil"
+        elif self.ruedas == 2:
+            return "motocicleta"
 
 
 
-def mostrar(self):
-    print("marca: {} , ruedas: {}, color: {}".format(self.marca, self.ruedas, self.color))
+    def mostrar(self):
+        print("marca: {} , ruedas: {}, color: {}, tipo de vehiculo: {}".format(self.nombre, self.ruedas, self.color, auto1.tipoVehiculo()))
 
 
-auto1=Vehiculo("fiat1", 4, "rojo")
+auto1=Vehiculo("fiat1", 4, "rojo") #objeto
 auto1.mostrar()
+
+"""
+Comisión: com 6/7
+Grupo: 8
+Integrantes:
+-51709 Avendaño, Cintia Esther
+-51658 Bernardi, Mateo
+-51671 Rodriguez, Joaquin 
+-51720 Novo, Luisina 
+"""
